2cd2f3830dc33bd8209c55ee150e53b5c8536f98
Kiana <s.kiana.hosseini@gmail.com>
2018-12-12 21:15:38 -0800

    ps0 solution

---
+++ ps0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+MIT: intro to computer science and programming in Python
+PS0
+Created on Fri Dec  7 15:20:06 2018
+@author: Kiooola
+"""
+import math
+
+x = input("Enter number x:")
+y = input ("Enter number y:")
+x = int(x)
+y = int (y)
+print ("x**y= ",x**y)
+z = math.log(x,2)
+print ("log(x)= ", z)
